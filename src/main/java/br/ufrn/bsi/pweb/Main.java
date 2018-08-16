c630501dd16dd739eb15b60b10ddcbe9ed5663fc
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-08-16 13:41:13 -0300

    codigo inicial

---
+++ src/main/java/br/ufrn/bsi/pweb/Main.java
@@ -0,0 +1,10 @@
+package br.ufrn.bsi.pweb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("executando Classe Main!!!");
+	}
+
+}
